fc0a7a82299ea9755e177ffc7c0870ba70518f9f
Lucas Nabesima <lucas.nabesima@gmail.com>
2021-10-24 18:11:10 -0300

    Consume Github API to customize NLW badge

--- main.js
+++ main.js
@@ -10,8 +10,23 @@ function changeSocialMediaLinks() {
   for (let li of socialLinks.children) {
     const social = li.getAttribute('class');
 
-    li.children[0].href = `https://${social}.com/${userSocialMedia[social]}`
+    li.children[0].href = `https://${social}.com/${userSocialMedia[social]}`;
   }
 }
 
+function getGithubProfileInfo() {
+  const url = `https://api.github.com/users/${userSocialMedia.github}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name;
+      userLink.href = data.html_url;
+      userBio.textContent = data.bio;
+      userAvatar.src = data.avatar_url;
+      userLogin.textContent = data.login;
+    });
+}
+
 changeSocialMediaLinks();
+getGithubProfileInfo();
